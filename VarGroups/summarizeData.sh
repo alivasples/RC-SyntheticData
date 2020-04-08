9caaaf35d6ac9cb6639dc46a65facefa585b82b2
Alexis Aspauza <alivasples@gmail.com>
2020-04-07 22:17:58 -0300

    .

---
+++ VarGroups/summarizeData.sh
@@ -0,0 +1,52 @@
+cat 1/fts_all.result 2/fts_all.result 3/fts_all.result 4/fts_all.result 5/fts_all.result 6/fts_all.result 7/fts_all.result 8/fts_all.result 9/fts_all.result 10/fts_all.result > fts_all.result
+
+rm 1/fts_all.result
+rm 2/fts_all.result
+rm 3/fts_all.result
+rm 4/fts_all.result
+rm 5/fts_all.result
+rm 6/fts_all.result
+rm 7/fts_all.result
+rm 8/fts_all.result
+rm 9/fts_all.result
+rm 10/fts_all.result
+
+cat 1/fts_any.result 2/fts_any.result 3/fts_any.result 4/fts_any.result 5/fts_any.result 6/fts_any.result 7/fts_any.result 8/fts_any.result 9/fts_any.result 10/fts_any.result > fts_any.result
+
+rm 1/fts_any.result
+rm 2/fts_any.result
+rm 3/fts_any.result
+rm 4/fts_any.result
+rm 5/fts_any.result
+rm 6/fts_any.result
+rm 7/fts_any.result
+rm 8/fts_any.result
+rm 9/fts_any.result
+rm 10/fts_any.result
+
+cat 1/index_all.result 2/index_all.result 3/index_all.result 4/index_all.result 5/index_all.result 6/index_all.result 7/index_all.result 8/index_all.result 9/index_all.result 10/index_all.result > index_all.result
+
+rm 1/index_all.result
+rm 2/index_all.result
+rm 3/index_all.result
+rm 4/index_all.result
+rm 5/index_all.result
+rm 6/index_all.result
+rm 7/index_all.result
+rm 8/index_all.result
+rm 9/index_all.result
+rm 10/index_all.result
+
+cat 1/index_any.result 2/index_any.result 3/index_any.result 4/index_any.result 5/index_any.result 6/index_any.result 7/index_any.result 8/index_any.result 9/index_any.result 10/index_any.result > index_any.result
+
+rm 1/index_any.result
+rm 2/index_any.result
+rm 3/index_any.result
+rm 4/index_any.result
+rm 5/index_any.result
+rm 6/index_any.result
+rm 7/index_any.result
+rm 8/index_any.result
+rm 9/index_any.result
+rm 10/index_any.result
+
