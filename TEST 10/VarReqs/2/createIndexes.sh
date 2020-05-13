f7bf5ef011920166698f02210a5f99f711c73fac
alivasples <aliv.asples@gmail.com>
2020-05-13 00:57:42 -0300

    Test 10 and 11

---
+++ TEST 10/VarReqs/2/createIndexes.sh
@@ -0,0 +1,4 @@
+../../../../../Eclipse/IndexCreator/Debug/IndexCreator T1.data 0 simple
+../../../../../Eclipse/IndexCreator/Debug/IndexCreator T1.data 1 simple
+../../../../../Eclipse/IndexCreator/Debug/IndexCreator T1.data 2 complex
+../../../../../Eclipse/IndexCreator/Debug/IndexCreator T1.data 3 complex
